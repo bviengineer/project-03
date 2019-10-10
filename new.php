d502d85cb05a7d987971b629f0ed95b5e8f45cac
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-10 19:13:23 -0400

    Update new.php
    Remoed header and footer tags and included header.php and footer.php, respectively

--- new.php
+++ new.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>MyJournal</title>
-        <link href="https://fonts.googleapis.com/css?family=Cousine:400" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:600" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/site.css">
-    </head>
+<?php include 'inc/header.php'; ?>
     <body>
         <header>
             <div class="container">
@@ -40,10 +29,4 @@
                 </div>
             </div>
         </section>
-        <footer>
-            <div>
-                &copy; MyJournal
-            </div>
-        </footer>
-    </body>
-</html>
+        <?php include 'inc/footer.php'; ?>
