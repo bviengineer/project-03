10127d9819d9bda680c79a2d97aeb1fac104a35e
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-11 14:44:08 -0400

    Update dbconnection.php
    Remove echo statement.

--- inc/dbconnection.php
+++ inc/dbconnection.php
@@ -2,8 +2,7 @@
     try {
         $db = new PDO("sqlite:".__DIR__."/journal.db");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
-        echo "connected to the database"; //testing
-    } catch (Exception $e) {
+        } catch (Exception $e) {
         echo $e->getMessage();
         exit;
     }
