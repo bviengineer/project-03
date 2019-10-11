90208b6b7a5e961c0193f6455e7b4d9e23fdb9e7
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-11 11:36:21 -0400

    Update dbconnection.php
    Update db connection object path

--- inc/dbconnection.php
+++ inc/dbconnection.php
@@ -1,6 +1,6 @@
 <?php 
     try {
-        $db = new PDO('sqlite:__DIR__journal.db');
+        $db = new PDO("sqlite:".__DIR__."/journal.db");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
         echo "connected to the database"; //testing
     } catch (Exception $e) {
