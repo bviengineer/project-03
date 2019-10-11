d90766d7044097d7cf87ea0f907cef77d58fc708
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-11 14:27:59 -0400

    Update index.php
    Update additional URLS

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
     include 'inc/header.php';
+
+
 ?>
     <body>
         <header>
@@ -14,19 +16,19 @@
             <div class="container">
                 <div class="entry-list">
                     <article>
-                        <h2><a href="detail.html">The best day I’ve ever had</a></h2>
+                        <h2><a href="detail.php">The best day I’ve ever had</a></h2>
                         <time datetime="2016-01-31">January 31, 2016</time>
                     </article>
                     <article>
-                        <h2><a href="detail_2.html">The absolute worst day I’ve ever had</a></h2>
+                        <h2><a href="detail_2.php">The absolute worst day I’ve ever had</a></h2>
                         <time datetime="2016-01-31">January 31, 2016</time>
                     </article>
                     <article>
-                        <h2><a href="detail_3.html">That time at the mall</a></h2>
+                        <h2><a href="detail_3.php">That time at the mall</a></h2>
                         <time datetime="2016-01-31">January 31, 2016</time>
                     </article>
                     <article>
-                        <h2><a href="detail_4.html">Dude, where’s my car?</a></h2>
+                        <h2><a href="detail_4.php">Dude, where’s my car?</a></h2>
                         <time datetime="2016-01-31">January 31, 2016</time>
                     </article>
                 </div>
