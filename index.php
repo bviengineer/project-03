f5b815c3858c946d250e0a9a6b9a3822881e7678
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-11 22:56:24 -0400

    Update index.php
    Retrieve & display entries from the database

--- index.php
+++ index.php
@@ -1,6 +1,52 @@
 <?php 
-    include 'inc/header.php';
+	// Including the file with the header tags and the database connection
+	include 'inc/header.php';
+	include 'inc/dbconnection.php';
 
+	// Retrieving entries from the database
+	try {
+			$results = $db->query("SELECT * FROM entries"); //TRY ADDING LIMIT 2 
+	} catch (Exception $e) {
+			echo $e->getMessage();
+			die();
+	}
+
+	// Assigning PDOStatement object result set to a variable
+	$entries = $results->fetchAll(PDO::FETCH_ASSOC);
+	echo '<pre>';
+	//var_dump($entries);
+	echo '</pre>';
+
+	// Looping through the array of entries & printing title and date to the index page
+	function print_entries(){	
+		global $entries;
+
+		foreach ($entries as $entry) {	
+			$title = strtolower($entry['title']);
+			echo $title . "<br>";
+			echo "<h2><a href='";
+
+			if ($title == strtolower("The best day I ever had")) {
+				echo "detail.php' >";
+
+			} elseif ($title == strtolower("The absolute worst day I’ve ever had")) {
+				echo "detail_2.php' >";
+
+			} elseif ($title == strtolower("That time at the mall")) {
+				echo "detail_3.php' >";
+
+			} elseif ($title == strtolower("Dude, where’s my car?")) {
+				echo "detail_4.php' >";
+			} else {
+					echo "#";
+			}
+
+			echo $entry["title"] . "</a></h2> <br>";
+			echo "<time datetime=" . $entry["date"] . '>' . $entry["date"] . "</time> <br>";
+		}
+	}
+
+	print_entries();
 
 ?>
     <body>
@@ -16,11 +62,11 @@
             <div class="container">
                 <div class="entry-list">
                     <article>
-                        <h2><a href="detail.php">The best day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
+										<h2><a href="detail.php">The best day I ever had</a></h2>
+                        <time datetime="2018-01-31">January 31, 2016</time>
                     </article>
                     <article>
-                        <h2><a href="detail_2.php">The absolute worst day I’ve ever had</a></h2>
+                        <h2><a href="detail_2.html">The absolute worst day I’ve ever had</a></h2>
                         <time datetime="2016-01-31">January 31, 2016</time>
                     </article>
                     <article>
