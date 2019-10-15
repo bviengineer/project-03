f03ec93302bc049fa41493ee400d24cd6927f655
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-15 19:18:59 -0400

    Update new.php
    Update SQL insert query; prepared input values

--- new.php
+++ new.php
@@ -6,30 +6,42 @@
     if (!empty($_POST['title'])) {
         //use trim() function to remove whites pace before and after 
       $title = $_POST['title'];
-      $date = $_POST['date'];
+      $date_written = $_POST['date'];
       $time_spent = $_POST['timeSpent'];
       $learned = $_POST['whatILearned'];
 			$resources = $_POST['ResourcesToRemember'];
 			add_journal_entry();
-			// echo '<pre>';
-			// 	var_dump($_POST['title']);
-			// 	var_dump($_POST['date']);
-			// 	var_dump($_POST['timeSpent']);
-			// 	var_dump($_POST['whatILearned']);
-			// 	var_dump($_POST['ResourcesToRemember']);
-			// echo '</pre>';
-      //echo "<br>Your title is: " . $title . "<br>";
+			echo '<pre>';
+				var_dump($_POST['title']);
+				var_dump($_POST['date']);
+				var_dump($_POST['timeSpent']);
+				var_dump($_POST['whatILearned']);
+				var_dump($_POST['ResourcesToRemember']);
+			echo '</pre>';
+      echo "<br>Your title is: " . $title . "<br>";
     } elseif($_POST && empty($_POST['title'])) {
-				echo "you didn't enter any data";
+				$blank_form_err = "You didn't enter any data";
 		}
 
 		// Function will add a new journal entry to the database
-    function add_journal_entry($title, $date, $time_spent, $learned, $resources){
+    function add_journal_entry($title, $date, $time_spent = NULL, $learned, $resources = NULL){
         include 'inc/dbconnection.php';
         
-				// $addEntry = "INSERT INTO entries ($title, $date, $time_spent, $learned, $resources) VALUES($title, $date, $time_spent, $learned, $resources)";
-				
-				"INSERT INTO entries ($title, $date, $time_spent, $learned, $resources) VALUES($title, $date, $time_spent, $learned, $resources)";
+				$addEntry = "INSERT INTO entries (title, date_written, time_spent, learned, resources) VALUE(?, ?, ?, ?, ?)";
+
+				try {
+					$results = $db->prepare($addEntry);
+					$results->bindValue(1, $title, PDO::PARAM_STR);
+					$results->bindValue(2, $date_written, PDO::PARAM_STR);
+					$results->bindValue(3, $time_spent, PDO::PARAM_INT);
+					$results->bindValue(4, $learned, PDO::PARAM_STR);
+					$results->bindValue(5, $resources, PDO::PARAM_STR);
+					$results->execute();
+					echo "addition successful";
+
+				} catch (Exception $e) {
+						$e->getMessage();
+				}
     }
 ?>
     <body>
@@ -45,6 +57,7 @@
             <div class="container">
                 <div class="new-entry">
                     <h2>New Entry</h2>
+										<?php echo $blank_form_err; ?>
                     <form method="POST" action="#">
                         <label for="title"> Title</label>
                         <input id="title" type="text" name="title"><br>
