caa98d007876497030ce9a58e3c83ec6c88254a0
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-16 12:07:53 -0400

    Update index.php
    Function to print journal entries to the page

--- index.php
+++ index.php
@@ -17,39 +17,16 @@
 	//var_dump($entries);
 	echo '</pre>';
 
-	// Looping through the array of entries & printing title and date to the index page
-	// function print_entries(){	
-	// 	global $entries;
-	
-	// 	foreach ($entries as $entry) {	
-	// 		$title = strtolower($entry['title']);
+	function get_journal_entries() {
+		global $entries; // Grants function access to the $entries variable 
 
-	// 		echo "<h2><a href=' ";
-	// 		if ($title == strtolower("The best day I ever had") ) {
-	// 				echo "detail.php' >";
-
-	// 		} elseif ($title == strtolower("The absolute worst day I’ve ever had")) {
-	// 				echo "detail_2.php' >";
-
-		 //} elseif ($title == strtolower("That time at the mall")) {
-			// 	echo "detail_3.php' >";
-
-			// } elseif ($title == strtolower("Dude, where’s my car?")) {
-			// 	echo "detail_4.php' >";
-			// } else {
-			// 		echo "#";
-			// }
-			//echo "detail.php' >";
-			//echo $entry['title'];
-			//echo $entry["title"] . "</a></h2>";
-			//echo "</a></h2>";
-			// echo "<time datetime=" . $entry["date"] . '>' . $entry["date"] . "</time> <br>";
-			//}
-		//}
-	//}
-
-	//print_entries();
-
+		foreach ($entries as $entry) {
+			echo "<h2><a href='detail.php?id= ";
+			echo $entry['id'] . '>';
+			echo $entry['title'];
+			echo "</a></h2>";
+		}
+	}
 ?>
     <body>
         <header>
@@ -64,20 +41,9 @@
             <div class="container">
                 <div class="entry-list">
                     <article>
-						<h2><a href="detail.php"><?php echo $entries[0]['title']; ?> </a></h2>
-                        <time datetime=<?php echo $entries[0]['date']; ?> > <?php strtotime($entries[0]['date']); ?></time> <!-- "2018-01-31" January 31, 2016-->
-                    </article>
-                    <article>
-                        <h2><a href="detail_2.html"><?php echo $entries[1]['title']; ?></a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_3.php"> <?php echo $entries[2]['title']; ?></a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_4.php"><?php echo $entries[3]['title']; ?></a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
+					<?php 
+						get_journal_entries();
+					?>	
                     </article>
                 </div>
             </div>
