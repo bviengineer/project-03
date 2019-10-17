cdfdb65aa9f8c724b07e7daa7d93a0a04aa14fea
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-17 11:07:46 -0400

    Update index.php
    Move function to print journal entries into dedicaetd functions file

--- index.php
+++ index.php
@@ -2,28 +2,6 @@
 	// Inclusion of files containing header tags and the database connection
 	include 'inc/header.php';
 	include 'inc/functions.php';
-
-	// Assignment of return values or PDOStatement object result set to a variable
-	//$entries = $results->fetchAll(PDO::FETCH_ASSOC);
-	echo '<pre>';
-	//var_dump($entries);
-	echo '</pre>';
-
-	// Print journal entries to the index page
-	function print_journal_entries() {
-		//global $entries; // Grants function access to the $entries variable 
-
-		foreach (get_journal_entries() as $entry) {
-			echo "<h2><a href='detail.php?id= ";
-			echo $entry['id'] . " '> ";
-			echo $entry['title'];
-			echo "</a></h2>";
-			echo "<time>";
-			echo $entry['date'];
-			echo "</time>";
-			echo "<hr>";
-		}
-	}
 ?>
     <body>
         <header>
