22d5b108ccbaca09cefbcde1267ab52152ace85d
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-17 09:41:14 -0400

    Create functions.php
    Fiile will hold all application functions (separation of concerns ).

---
+++ inc/functions.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+	This file will hold all functions needed to run the application
+*/
+
+
+// Add a new journal entry to the database
+function add_journal_entry($title, $date = NULL, $time_spent = NULL, $learned = NULL, $resources = NULL){
+    include 'inc/dbconnection.php';
+    
+    // Insert journal entry into database sql statement 
+    $add_entry = "INSERT INTO entries (title, date, time_spent, learned, resources) VALUES(?, ?, ?, ?, ?)";
+
+    try {
+        $results = $db->prepare($add_entry); // Prepare sql statenebt abd palace the results into the variable $results
+        $results->bindValue(1, $title, PDO::PARAM_STR); // Associates the 1st ? with the $title var
+        $results->bindValue(2, $date, PDO::PARAM_STR); // Associates the 2nd ? with the $date var
+        $results->bindValue(3, $time_spent, PDO::PARAM_INT); // Associates the 3rd ? with the $time_spent var
+        $results->bindValue(4, $learned, PDO::PARAM_STR); // Associates the 4th ? with the $learned var
+        $results->bindValue(5, $resources, PDO::PARAM_STR); // Associates the 5th ? with the $resources var
+        $results->execute(); // Executes the insert query after filtering & binding the inputs
+         echo "Your journal entry was added successfully!"; // Prints confiramtion msg to the screen after adding entry to database
+
+    } catch (Exception $e) {
+            $e->getMessage();
+            return array();
+    }
+}
+
