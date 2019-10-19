10b98436c577961c88bef5f1c3cf148ab302fa38
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-19 07:23:37 -0400

    Update edit.php
    UPDATE edit mode to verify that a POST is taking place before the database edit occurs.

--- edit.php
+++ edit.php
@@ -10,20 +10,23 @@
 	// echo "<br> the title passed from GET is: ";
 	// var_dump($edit_entry['title']);
 
-	if (!empty($edit_entry['title'])) {
-		echo "i'm in side the POST PART";
+	if (!empty($edit_entry['title']) && isset($edit_entry['id'])) {
+		//echo "i'm in side the POST PART";
 		//use trim() function to remove whites pace before and after ?
+		if ($_POST) {
+			var_dump($_POST);
 		// $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
 		// $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
 		// $time_spent = filter_input(INPUT_POST, 'timeSpent', FILTER_SANITIZE_NUMBER_INT);
 		// $learned = filter_input(INPUT_POST, 'whatILearned', FILTER_SANITIZE_STRING);
 		// $resources = filter_input(INPUT_POST, 'ResourcesToRemember', FILTER_SANITIZE_STRING);
 	
-			// if (update_journal_entry($title, $date, $time_spent, $learned, $resources, $id=$edit_entry['id'])) {
+			// if (update_journal_entry($title, $date, $time_spent, $learned, $resources)) {
 			// 	header('Location: index.php');
 			// 	exit;
 			// }
 		}
+	}
 ?>
     <body>
         <header>
@@ -38,7 +41,7 @@
             <div class="container">
                 <div class="edit-entry">
                     <h2>Edit Entry</h2>
-                    <form method="POST" action="">
+                    <form method="POST" action="#">
                         <label for="title"> Title</label>
                         <input id="title" type="text" name="title" value="<?php echo $edit_entry['title']; ?>"><br>
                         
