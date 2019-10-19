4938afc4559a8cadb7d3271f4b4ef227b000cf67
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-19 06:54:32 -0400

    Update edit.php
    Edit mode is able to access code to be edited

--- edit.php
+++ edit.php
@@ -5,6 +5,25 @@
 	if (isset($_GET['id'])) {
 		$edit_entry = get_single_entry($_GET['id']);
 	}
+	// echo 'the get ID is: ';
+	// var_dump($_GET['id']);
+	// echo "<br> the title passed from GET is: ";
+	// var_dump($edit_entry['title']);
+
+	if (!empty($edit_entry['title'])) {
+		echo "i'm in side the POST PART";
+		//use trim() function to remove whites pace before and after ?
+		// $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
+		// $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
+		// $time_spent = filter_input(INPUT_POST, 'timeSpent', FILTER_SANITIZE_NUMBER_INT);
+		// $learned = filter_input(INPUT_POST, 'whatILearned', FILTER_SANITIZE_STRING);
+		// $resources = filter_input(INPUT_POST, 'ResourcesToRemember', FILTER_SANITIZE_STRING);
+	
+			// if (update_journal_entry($title, $date, $time_spent, $learned, $resources, $id=$edit_entry['id'])) {
+			// 	header('Location: index.php');
+			// 	exit;
+			// }
+		}
 ?>
     <body>
         <header>
