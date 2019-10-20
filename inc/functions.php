ab79de8ee9a9f3b39c7e5e911d703cdf02aa56b9
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-20 12:39:08 -0400

    Update functions.php
    Code to handle date conversion format as MONTH DAY, YEAR

--- inc/functions.php
+++ inc/functions.php
@@ -21,12 +21,68 @@ function get_journal_entries() {
 function print_journal_entries() {
 
 	foreach (get_journal_entries() as $entry) {
+		$date_conversion = explode("-", $entry['date']); // Converts date of type string to an array, - is the separator 
+
+		$month = ''; // At index 1 of $date_conversion is the month
+		$day = $date_conversion[2]; // At index 2 of $date_conversion is the day
+		$year = $date_conversion[0]; // At index 0 of $date_conversion is the year
+
+		// Will assign the month [in words] to the $month variable before printing to the page
+		switch ($date_conversion[1]) {
+			case '1':
+			$month = 'January';
+			break;
+
+			case '2':
+				$month = 'February';
+				break;
+			
+				case '3':
+				$month = 'March';
+				break;
+
+				case '4':
+				$month = 'April';
+				break;
+
+				case '5':
+				$month = 'May';
+				break;
+
+				case '6':
+				$month = 'June';
+				break;
+
+				case '7':
+				$month = 'July';
+				break;
+
+				case '8':
+				$month = 'August';
+				break;
+
+				case '9':
+				$month = 'September';
+				break;
+
+				case '10':
+				$month = 'October';
+				break;
+
+				case '11':
+				$month = 'November';
+				break;
+
+				case '12':
+				$month = 'December';
+				break;
+		}
 		echo "<h2><a href='detail.php?id=";
 		echo $entry['id'] . " '> ";
 		echo $entry['title'];
 		echo "</a></h2>";
 		echo "<time>";
-		echo $entry['date'];
+		echo $month . ' ' . $day . ', ' . $year; 
 		echo "</time>";
 		echo "<hr>";
 	}
