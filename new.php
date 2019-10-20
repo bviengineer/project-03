a441d55f0a24cb3b6602e336f247c07ac158d24f
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-20 10:41:04 -0400

    Update new.php
    -Add <br> tags in error message output
    -Comment out date validation (not needed)
    -Update name of error message function

--- new.php
+++ new.php
@@ -26,17 +26,24 @@
 														$resources = trim(filter_input(INPUT_POST, 'ResourcesToRemember', FILTER_SANITIZE_STRING));
 													
 														$dateMatch = explode('-',$date);
-														var_dump($dateMatch);
+														//var_dump($dateMatch);
 													
 														// Will ensure required fileds are completed before adding entry to the database
 														if (empty($title) || empty($date) || empty($time_spent) || empty($learned)) {
-																echo print_blank_err_msg("Please ensure the:<br> Title, Date, Time Spent & What I learned fields <br> are completed in order to save this entry.");
-														} else {
+																echo print_err_msg("Please ensure the:<br> Title, Date, Time Spent & What I learned fields <br> are completed in order to save this entry");
+														} /*elseif (count($dateMatch) !=3 || 
+																			strlen($dateMatch[0]) != 4 || //year
+																			strlen($dateMatch[1]) != 2 || //month
+																			strlen($dateMatch[2]) != 2 || //day
+																			!checkdate($dateMatch[1], $dateMatch[2], $dateMatch[0])) {
+																
+																			echo print_err_msg("Invalid Date");
+														}*/ else {
 																if (add_journal_entry($title, $date, $time_spent, $learned, $resources)) {
 																	header('Location: index.php?msg=Your+journal+entry+was+added+!');
 																	exit;
 															} else {
-																	echo print_blank_err_msg("Could not add journal entry. Please try again!");
+																	echo print__err_msg("Could not add journal entry. Please try again!");
 															}
 														}
 													}
