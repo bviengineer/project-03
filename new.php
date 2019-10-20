04c9a5d65250b8a25b0514096463bf8d23809e49
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-20 09:58:38 -0400

    Update new.php
    -Add data validation for additionl form fields
    -Relocate code block inside document

--- new.php
+++ new.php
@@ -2,44 +2,46 @@
 	// Inclusion of files containing header tags and all functions needed to run the application
 	include 'inc/head.php'; 
 	include 'inc/functions.php';
-
-	// Conditional will ensure there's at least a title for a given entry before adding it to the database
-	if (!empty($_POST['title'])) {
-	//use trim() function to remove whites pace before and after ?
-	$title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
-	$date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
-	$time_spent = filter_input(INPUT_POST, 'timeSpent', FILTER_SANITIZE_NUMBER_INT);
-	$learned = filter_input(INPUT_POST, 'whatILearned', FILTER_SANITIZE_STRING);
-	$resources = filter_input(INPUT_POST, 'ResourcesToRemember', FILTER_SANITIZE_STRING);
-
-		if (add_journal_entry($title, $date, $time_spent, $learned, $resources)) {
-				header('Location: index.php?msg=Your+journal+entry+was+added+!');
-				exit;
-		} else {
-				header('Location: index.php?msg="Could+not+add+journal+entry+.+Please+try+again!');
-				exit;
-		}
-	}
 ?>
 	<body>
         <header>
             <div class="container">
                 <div class="site-header">
                     <a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
-                    <a class="button icon-right" href="new.html"><span>New Entry</span> <i class="material-icons">add</i></a>
+                    <a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
                 </div>
             </div>
-        </header>
+				</header>
         <section>
             <div class="container">
                 <div class="new-entry">
-                    <h2>New Entry</h2>
-										<!-- Will print error message to the screen if title field of the form is blank -->
-										<?php 
-											if ($_POST && empty($_POST['title'])) {
-												echo print_blank_err_msg("You need at least a title in order to save an entry.");
-											} 
-										?>
+										<h2>New Entry</h2>
+										<h5>
+											<?php 
+													if ($_POST) {
+														$title = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING));
+														$date = trim(filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING));
+														$time_spent = trim(filter_input(INPUT_POST, 'timeSpent', FILTER_SANITIZE_NUMBER_INT));
+														$learned = trim(filter_input(INPUT_POST, 'whatILearned', FILTER_SANITIZE_STRING));
+														$resources = trim(filter_input(INPUT_POST, 'ResourcesToRemember', FILTER_SANITIZE_STRING));
+													
+														$dateMatch = explode("/", $date);
+														//var_dump($dateMatch);
+													
+														// Will ensure required fileds are completed before adding entry to the database
+														if (empty($title) || empty($date) || empty($time_spent) || empty($learned)) {
+																echo print_blank_err_msg("Please ensure the:<br> Title, Date, Time Spent & What I learned fields <br> are completed in order to save this entry.");
+														} else {
+																if (add_journal_entry($title, $date, $time_spent, $learned, $resources)) {
+																	header('Location: index.php?msg=Your+journal+entry+was+added+!');
+																	exit;
+															} else {
+																	echo print_blank_err_msg("Could not add journal entry. Please try again!");
+															}
+														}
+													}
+											?>
+										</h5>
                     <form method="POST" action="">
                         <label for="title"> Title</label>
                         <input id="title" type="text" name="title"><br>
