b2e4815bded5cde57229c58515caaf0609798f5d
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-22 14:24:06 -0400

    Update index.php
    Fix indentation

--- index.php
+++ index.php
@@ -3,34 +3,34 @@
 	include 'inc/head.php';
   include 'inc/functions.php';
 ?>
-  <body>
-        <header>
-            <div class="container">
-                <div class="site-header">
-                    <a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
-                    <a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
-                </div>
-            </div>
-        </header>
-        <section>
-            <div class="container">
-							<h6>
-								<?php
-									if (isset($_GET['msg'])) {
-										echo $_GET['msg'];
-										} else {
-											echo '';
-										}
-									?>
-							</h6>
-							<div class="entry-list">
-                    <article>
-											<?php 
-												// Will print the journal entries from the database, to the page
-												print_journal_entries();
-											?>	
-                    </article>
-                </div>
-            </div>
-        </section>
+<body>
+	<header>
+  	<div class="container">
+			<div class="site-header">
+				<a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
+				<a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
+			</div>
+    </div>
+  </header>
+	<section>
+		<div class="container">
+			<h6>
+				<?php
+					if (isset($_GET['msg'])) {
+						echo $_GET['msg'];
+					} else {
+						echo '';
+					}
+				?>
+			</h6>
+			<div class="entry-list">
+				<article>
+					<?php 
+						// Will print the journal entries from the database, to the page
+						print_journal_entries();
+					?>	
+				</article>
+			</div>
+		</div>
+	</section>
 <?php include 'inc/footer.php';?>
