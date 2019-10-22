39fc9b21a7a145c9397fd0fc1719e9f8a04b280d
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-22 15:21:27 -0400

    Update detail.php
    Fix indentation

--- detail.php
+++ detail.php
@@ -20,47 +20,48 @@
 		}
 	}
 ?>
-    <body>
-        <header>
-            <div class="container">
-                <div class="site-header">
-                    <a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
-                    <a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
-                </div>
-            </div>
-        </header>
-        <section>
-            <div class="container">
-                <div class="entry-list single">
-                    <article>
-												<!-- Printing of journal entry details to the page  -->
-												<h1><?php echo $single_entry['title']; ?></h1>
-                        <time datetime="2016-01-31"><?php echo date('F d, Y', strtotime($single_entry['date'])); ?></time>
-                        <div class="entry">
-                            <h3>Time Spent: </h3>
-                            <p><?php echo $single_entry['time_spent']; ?> minutes</p>
-                        </div>
-                        <div class="entry">
-                            <h3>What I Learned:</h3>
-                            <p><?php echo $single_entry['learned']; ?></p>
-                        </div>
-                        <div class="entry">
-                            <h3>Resources to Remember:</h3>
-                            <p><?php echo $single_entry['resources']; ?></p>
-                        </div>
-                    </article>
-                </div>
-            </div>
-            <div class="edit">
-								<!-- Pass request to edit journal entry to the edit.php page -->
-                <p><a href="edit.php?id=<?php echo $single_entry['id'];?>">Edit Entry</a></p>
-
-								<!-- From with delete button -->
-								<form class='delete-form' 
-								method="POST" action="">
-									<label for="delete-entry">&#128073; CANNOT BE UNDONE &#128400;</label>
-									<input type="submit" class="delete-btn" name="deleteEntry" value="Delete Entry"> 
-								</form>
-            </div>
-        </section>
-				<?php include 'inc/footer.php'; ?>
+<body>
+	<header>
+		<div class="container">
+			<div class="site-header">
+				<a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
+				<a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
+			</div>
+		</div>
+	</header>
+	<section>
+		<div class="container">
+			<div class="entry-list single">
+				<article>
+					<!-- Printing of journal entry details to the page  -->
+					<h1><?php echo $single_entry['title']; ?></h1>
+					<time datetime="2016-01-31"><?php echo date('F d, Y', strtotime($single_entry['date'])); ?></time>
+					<div class="entry">
+							<h3>Time Spent: </h3>
+							<p><?php echo $single_entry['time_spent']; ?> minutes</p>
+					</div>
+					<div class="entry">
+						<h3>What I Learned:</h3>
+						<p><?php echo $single_entry['learned']; ?></p>
+					</div>
+					<div class="entry">
+						<h3>Resources to Remember:</h3>
+						<p><?php echo $single_entry['resources']; ?></p>
+					</div>
+					<div> 
+						<p class="tags">Tags: <a href="filtered_entries.php?tag="><?php ?>tags</p>
+					</div>
+				</article>
+			</div>
+		</div>
+		<div class="edit">
+			<!-- Pass request to edit journal entry to the edit.php page -->
+			<p><a href="edit.php?id=<?php echo $single_entry['id'];?>">Edit Entry</a></p>
+			<!-- From with delete button -->
+			<form class='delete-form' method="POST" action="">
+				<label for="delete-entry">&#128073; CANNOT BE UNDONE &#128400;</label>
+				<input type="submit" class="delete-btn" name="deleteEntry" value="Delete Entry"> 
+			</form>
+		</div>
+	</section>
+<?php include 'inc/footer.php'; ?>
