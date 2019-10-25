6bfbd16b6254449379d2c999a01315387ac5f8d7
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-25 16:32:51 -0400

    Update dbconnection.php
    Addition of statement to dbconnection file

--- inc/dbconnection.php
+++ inc/dbconnection.php
@@ -1,8 +1,15 @@
 <?php
     try {
         $db = new PDO("sqlite:".__DIR__."/journal.db");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        $db->exec( 'PRAGMA foreign_keys = ON;' ); // Obtained form Jennifer Nordell, see note below 
         } catch (Exception $e) {
         echo $e->getMessage();
         exit;
-    }
+		}
+		
+/*=================================
+	CRITICAL
+=================================*/
+  // $db->exec( 'PRAGMA foreign_keys = ON;' ) is critical. Without it, the cascading options in sqlite does
+  // not work. Source:  https://dba.stackexchange.com/questions/94134/on-delete-cascade-not-deleting-entry-for-foreign-key
