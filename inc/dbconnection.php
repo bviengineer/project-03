043d57e51bc1797ea7e3295229ca0c65ba04699f
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-25 16:36:17 -0400

    Update dbconnection.php
    Corrected typo

--- inc/dbconnection.php
+++ inc/dbconnection.php
@@ -2,7 +2,7 @@
     try {
         $db = new PDO("sqlite:".__DIR__."/journal.db");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        $db->exec( 'PRAGMA foreign_keys = ON;' ); // Obtained form Jennifer Nordell, see note below 
+        $db->exec( 'PRAGMA foreign_keys = ON;' ); // Obtained from Jennifer Nordell, see note below 
         } catch (Exception $e) {
         echo $e->getMessage();
         exit;
