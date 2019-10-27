47c254b99fcf777b297d1859e2898dedece05445
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-10-27 09:23:17 -0400

    Update functions.php
    Organize or list functions by purpose [ CRUD ]

--- inc/functions.php
+++ inc/functions.php
@@ -9,12 +9,49 @@
 /*========================
  	CREATE
 =========================*/
-
+// ADD JOURNAL ENTRY
+function add_journal_entry($title, $date = NULL, $time_spent = NULL, $learned = NULL, $resources = NULL){
+	include 'inc/dbconnection.php';
+	$add_entry = "INSERT INTO entries (title, date, time_spent, learned, resources) VALUES(?, ?, ?, ?, ?)";
+  try {
+		$results = $db->prepare($add_entry);
+		$results->bindValue(1, $title, PDO::PARAM_STR);
+		$results->bindValue(2, $date, PDO::PARAM_STR);
+		$results->bindValue(3, $time_spent, PDO::PARAM_INT);
+		$results->bindValue(4, $learned, PDO::PARAM_STR);
+		$results->bindValue(5, $resources, PDO::PARAM_STR);
+		$results->execute();
+  } catch (Exception $e) {
+			$e->getMessage();
+			return array();
+	} 
+	return true; 
+}
+// ADD TAG(S) FOR A NEW ENTRY TO THE DATABASE
+function add_tags() {
+	include 'inc/dbconnection.php';
+	// Gets ID of most recent entry entered added to the dbase
+	$id = get_last_entry();
+	// Assigns returned id from get_last_entry (which is an associative array) & converts the ID from a str to an int
+	$entry_id = intval($id['id']); 
+	foreach ($_POST['tags'] as $tag) {
+		$tag_id = intval($tag);
+		try {
+			$results = $db->prepare("INSERT INTO entry_tag (entry_id, tag_id) VALUES(?, ?)");
+			$results->bindValue(1, $entry_id, PDO::PARAM_STR);
+			$results->bindValue(2, $tag_id, PDO::PARAM_STR);
+			$results->execute();
+		} catch(Exception $e) {
+				$e->getMessage();
+				return array();
+		}	
+	}
+	return true;
+}
 /*========================
  	READ
 =========================*/
 // Retrieve all journal entires form entires table 
-// RETRIEVE ALL JOURNAL ENTRIES FROM ENTRIES TABLE
 function get_journal_entries_table() {
 	include 'inc/dbconnection.php';
 	$sql = "SELECT * FROM entries ORDER BY date DESC";
@@ -26,7 +63,7 @@ function get_journal_entries_table() {
 	}
 		return $results->fetchAll(PDO::FETCH_ASSOC);
 }
-// Retrieve all journal entires and tags (SQL JOIN) RETRIEVE ALL JOURNAL ENTRIES & TAGS (SQL JOIN)
+// Retrieve all journal entires and tags (SQL JOIN)
 function get_journal_entries() {
 	include 'inc/dbconnection.php';
 	$sql = "SELECT entries.id, entries.title, entries.date, entries.learned, entries.resources, my_tags.tags,entry_tag.entry_id, entry_tag.tag_id
@@ -82,15 +119,98 @@ function get_filtered_entries($tag) {
 	}
 	return $results->fetchAll(PDO::FETCH_ASSOC);
 }
-
+// RETRIEVE TAGS BY ENTRY
+function get_tags() {
+	include 'inc/dbconnection.php';
+	$get_tags = "SELECT my_tags.tag_id, entries.id, my_tags.tags, entries.title
+							FROM my_tags 
+							LEFT OUTER JOIN entry_tag 
+							ON entry_tag.tag_id = my_tags.tag_id
+							LEFT OUTER JOIN entries 
+							ON entries.id = entry_tag.entry_id";
+	try {
+		$results = $db->query($get_tags);
+	} catch (Exception $e) {
+			$e->getMessage();
+			return array();
+	}
+	return $results->fetchAll(PDO::FETCH_ASSOC);
+}
+// RETRIEVE MOST RECENT JOURNAL ENTRY ADDED
+function get_last_entry() {
+	include 'inc/dbconnection.php';
+	$select_last_entry = "SELECT id FROM entries ORDER BY id DESC LIMIT 1";
+	try {
+		$results = $db->query($select_last_entry);
+	} catch (Exception $e) {
+		$e->getMessage();
+		return array();
+	}
+	return $results->fetch(PDO::FETCH_ASSOC);
+}
 /*========================
  	UPDATE
 =========================*/
+// UPDATE JOURNAL ENTRY
+function update_journal_entry($title, $date, $time_spent, $learned, $resources = NULL) {
+	include 'inc/dbconnection.php';
+	$update_entry = "UPDATE entries 
+										SET title = ?, date = ?, time_spent = ?, learned = ?, resources = ? 
+										WHERE id = ?";
+	if (isset($_POST['id'])) {
+		try {
+				$results = $db->prepare($update_entry);
+				$results->bindValue(1, $title, PDO::PARAM_STR);
+				$results->bindValue(2, $date, PDO::PARAM_STR);
+				$results->bindValue(3, $time_spent, PDO::PARAM_INT);
+				$results->bindValue(4, $learned, PDO::PARAM_STR);
+				$results->bindValue(5, $resources, PDO::PARAM_STR);
+				$results->bindValue(6, $_POST['id'], PDO::PARAM_INT);
+				$results->execute();
+		} catch (Exception $e) {
+				$e->getMessage();
+			return array();
+		} 
+	}
+	return true;
+}
+// UPDATE TAGS FOR A GIVEN JOURNAL ENTRY
+function update_tags($id) {
+	include 'inc/dbconnection.php';
+		$entry_id = intval($id);
+		$update_tags = "UPDATE entry_tag SET tag_id = ? WHERE entry_id = ?";
+		foreach ($_POST['tags'] as $tag) {
+			try {
+				$results = $db->prepare($update_tags);
+				$results->bindValue(1, intval($tag), PDO::PARAM_STR);
+				$results->bindValue(2, $entry_id, PDO::PARAM_STR);
+				$results->execute();
+			} catch(Exception $e) {
+					$e->getMessage();
+					return array();
+				}	
+		}
+		return true;
+	}
 /*========================
  	DELETE
 =========================*/
-
-
+// DELETE JOURNAL ENTRY
+function delete_single_entry($id) {
+	include 'inc/dbconnection.php';
+	$delete_entry = "DELETE FROM entries WHERE id = ?"; 
+	if (isset($_GET['id'])) {
+		try {
+			$results = $db->prepare($delete_entry);
+			$results->bindValue(1, $id, PDO::PARAM_INT);
+			$results->execute();
+		} catch (Connection $e) {
+				$e->getMessage();
+				return array();
+		}
+	}
+	return true;
+}
 // PRINT ALL JOURNAL ENTRIES: on index.php & creates hyperlinks to respective entries 
 // function print_journal_entries() {
 // 	foreach (pair_entries_tags() as $entry) {
@@ -138,23 +258,6 @@ function print_entries_tags() {
 		echo "<hr>";	
 	}
 }
-// RETRIEVE TAGS BY ENTRY
-function get_tags() {
-	include 'inc/dbconnection.php';
-	$get_tags = "SELECT my_tags.tag_id, entries.id, my_tags.tags, entries.title
-							FROM my_tags 
-							LEFT OUTER JOIN entry_tag 
-							ON entry_tag.tag_id = my_tags.tag_id
-							LEFT OUTER JOIN entries 
-							ON entries.id = entry_tag.entry_id";
-	try {
-		$results = $db->query($get_tags);
-	} catch (Exception $e) {
-			$e->getMessage();
-			return array();
-	}
-	return $results->fetchAll(PDO::FETCH_ASSOC);
-}
 // PRINT JOURNAL ENTRIES BY TAG: on filtered_entries.php
 function print_filtered_entries($tag) {
 	foreach (get_filtered_entries($tag) as $entry) {
@@ -170,114 +273,6 @@ function print_filtered_entries($tag) {
 		echo $entry['tags'] . "</a></h4>";
 		echo "<hr>";
 	}
-}
-// ADD JOURNAL ENTRY
-function add_journal_entry($title, $date = NULL, $time_spent = NULL, $learned = NULL, $resources = NULL){
-	include 'inc/dbconnection.php';
-	$add_entry = "INSERT INTO entries (title, date, time_spent, learned, resources) VALUES(?, ?, ?, ?, ?)";
-  try {
-		$results = $db->prepare($add_entry);
-		$results->bindValue(1, $title, PDO::PARAM_STR);
-		$results->bindValue(2, $date, PDO::PARAM_STR);
-		$results->bindValue(3, $time_spent, PDO::PARAM_INT);
-		$results->bindValue(4, $learned, PDO::PARAM_STR);
-		$results->bindValue(5, $resources, PDO::PARAM_STR);
-		$results->execute();
-  } catch (Exception $e) {
-			$e->getMessage();
-			return array();
-	} 
-	return true; 
-}
-// ADD TAG(S) FOR A NEW ENTRY TO THE DATABASE
-function add_tags() {
-	include 'inc/dbconnection.php';
-	// Gets ID of most recent entry entered added to the dbase
-	$id = get_last_entry();
-	// Assigns returned id from get_last_entry (which is an associative array) & converts the ID from a str to an int
-	$entry_id = intval($id['id']); 
-	foreach ($_POST['tags'] as $tag) {
-		$tag_id = intval($tag);
-		try {
-			$results = $db->prepare("INSERT INTO entry_tag (entry_id, tag_id) VALUES(?, ?)");
-			$results->bindValue(1, $entry_id, PDO::PARAM_STR);
-			$results->bindValue(2, $tag_id, PDO::PARAM_STR);
-			$results->execute();
-		} catch(Exception $e) {
-				$e->getMessage();
-				return array();
-		}	
-	}
-	return true;
-}
-// RETRIEVE MOST RECENT JOURNAL ENTRY ADDED
-function get_last_entry() {
-	include 'inc/dbconnection.php';
-	$select_last_entry = "SELECT id FROM entries ORDER BY id DESC LIMIT 1";
-	try {
-		$results = $db->query($select_last_entry);
-	} catch (Exception $e) {
-		$e->getMessage();
-		return array();
-	}
-	return $results->fetch(PDO::FETCH_ASSOC);
-}
-// UPDATE JOURNAL ENTRY
-function update_journal_entry($title, $date, $time_spent, $learned, $resources = NULL) {
-	include 'inc/dbconnection.php';
-	$update_entry = "UPDATE entries 
-										SET title = ?, date = ?, time_spent = ?, learned = ?, resources = ? 
-										WHERE id = ?";
-	if (isset($_POST['id'])) {
-		try {
-				$results = $db->prepare($update_entry);
-				$results->bindValue(1, $title, PDO::PARAM_STR);
-				$results->bindValue(2, $date, PDO::PARAM_STR);
-				$results->bindValue(3, $time_spent, PDO::PARAM_INT);
-				$results->bindValue(4, $learned, PDO::PARAM_STR);
-				$results->bindValue(5, $resources, PDO::PARAM_STR);
-				$results->bindValue(6, $_POST['id'], PDO::PARAM_INT);
-				$results->execute();
-		} catch (Exception $e) {
-				$e->getMessage();
-			return array();
-		} 
-	}
-	return true;
-}
-// UPDATE TAGS FOR A GIVEN JOURNAL ENTRY
-function update_tags($id) {
-	include 'inc/dbconnection.php';
-		$entry_id = intval($id);
-		$update_tags = "UPDATE entry_tag SET tag_id = ? WHERE entry_id = ?";
-		foreach ($_POST['tags'] as $tag) {
-			try {
-				$results = $db->prepare($update_tags);
-				$results->bindValue(1, intval($tag), PDO::PARAM_STR);
-				$results->bindValue(2, $entry_id, PDO::PARAM_STR);
-				$results->execute();
-			} catch(Exception $e) {
-					$e->getMessage();
-					return array();
-				}	
-		}
-		return true;
-	}
-// DELETE JOURNAL ENTRY
-function delete_single_entry($id) {
-	include 'inc/dbconnection.php';
-	$delete_entry = "DELETE FROM entries WHERE id = ?"; 
-	if (isset($_GET['id'])) {
-		try {
-			$results = $db->prepare($delete_entry);
-			$results->bindValue(1, $id, PDO::PARAM_INT);
-			$results->execute();
-		} catch (Connection $e) {
-				$e->getMessage();
-				return array();
-		}
-	}
-	return true;
 }
 // ERROR NOTIFICATION FUNCTION 
 	//Will halt a request & print an error message if submitted journal entry form data is invalid
